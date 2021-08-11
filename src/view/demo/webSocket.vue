3897091117f8d4abea0528f26fa524ef0f9598ca
zengwenfeng <wf930522>
2021-08-11 14:31:10 +0800

    fix:socket请求url添加判断

--- src/view/demo/webSocket.vue
+++ src/view/demo/webSocket.vue
@@ -35,7 +35,8 @@ export default {
   methods: {
     init() {
       const _this = this
-      this.socket = io('ws://127.0.0.1:8010')
+      let urls = process.env.NODE_ENV === 'development' ? 'ws://127.0.0.1:8010' : 'ws://49.234.212.42/:8010'
+      this.socket = io(urls)
       this.socket.on('connection', function(socket) {
       });
       this.socket.on('chat message', async function(msg) {
