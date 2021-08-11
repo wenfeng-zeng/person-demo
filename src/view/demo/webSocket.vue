5e598be506abaf4bd7406f840705deb13f09b4f3
zengwenfeng <wf930522>
2021-08-11 14:56:52 +0800

    fix:socket请求url修改

--- src/view/demo/webSocket.vue
+++ src/view/demo/webSocket.vue
@@ -35,7 +35,7 @@ export default {
   methods: {
     init() {
       const _this = this
-      let urls = process.env.NODE_ENV === 'development' ? 'ws://127.0.0.1:8010' : 'wss://fz-person.cn/socket'
+      let urls = process.env.NODE_ENV === 'development' ? 'ws://127.0.0.1:8010' : 'wss://wfz-person.cn/socket'
       this.socket = io(urls)
       this.socket.on('connection', function(socket) {
       });
